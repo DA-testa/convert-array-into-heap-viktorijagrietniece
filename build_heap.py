185727b9f0a9d1e0c78f56a8a913c59871420565
viktorijagrietniece <123946342+viktorijagrietniece@users.noreply.github.com>
2023-03-18 13:57:32 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -5,34 +5,39 @@ def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
+    size = len(data)
+    
+    def sift_down(i)
+    
+        nonlocal swaps
+        max_index = i
+        left_child = 2 * i + 1
+        right_child = 2 * i + 2
+    
+        if left_child < size and data[left_child] < data [max_index]:
+            max_index = left_child
+        if right_child < size and data[right_child] < data [max_index]:
+            max_index = right_child
+        if i != max_index :
+            data[i], data[max_index] = data[max_index], data[i]
+            swaps.append(()i, max_index)
+            sift_down(max_index)
+            
+    for i in range(size // 2, -1, -1):
+        sift_down(i)
     return swaps
-
-
+        
+     
 def main():
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
+  
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
